78eca7a46dac6cf249471920a70ae0eb209b30fc
GavinAperano <audesandrine6@gmail.com>
2023-05-05 12:17:44 +0000

    build ok

--- wp-content/themes/gavinn/front-page.php
+++ wp-content/themes/gavinn/front-page.php
@@ -2,13 +2,8 @@
 get_header() ?>
 <?php while( have_posts()): the_post() ?>
        <h1> <?php the_title() ?> </h1>
-
        <?php the_content() ?>
-
        <a href="<?= get_post_type_archive_link('post') ?>"> See the latest news</a>
-
-
-
 <?php endwhile; ?>
         
 
