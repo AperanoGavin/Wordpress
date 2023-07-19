1f50969959b130b71a150f188113c14697074d79
audesandrine6@gmail.com <audesandrine6@gmail.com>
2023-07-19 19:37:42 +0000

    remove white style

--- wp-content/themes/theme-wdp-project/page-about-us.php
+++ wp-content/themes/theme-wdp-project/page-about-us.php
@@ -50,64 +50,38 @@
             <div class="col-md-3">
                 <img src="<?php echo get_theme_mod('service_image_1'); ?>" alt="Image 1">
                 <div class="service-info">
-                    <p style="color:white;"><?php echo get_theme_mod('service_image_1_title', 'Manager'); ?></p>
-                    <p style="color:white;"><?php echo get_theme_mod('service_image_1_phone', '+33 1 53 31 25 23'); ?></p>
-                    <p style="color:white;"><?php echo get_theme_mod('service_image_1_email', 'info@esgi.com'); ?></p>
+                    <p "><?php echo get_theme_mod('service_image_1_title', 'Manager'); ?></p>
+                    <p "><?php echo get_theme_mod('service_image_1_phone', '+33 1 53 31 25 23'); ?></p>
+                    <p "><?php echo get_theme_mod('service_image_1_email', 'info@esgi.com'); ?></p>
                 </div>
             </div>
             <div class="col-md-3">
                 <img src="<?php echo get_theme_mod('service_image_2'); ?>" alt="Image 2">
                 <div class="service-info">
-                    <p style="color:white;"><?php echo get_theme_mod('service_image_2_title', 'Manager'); ?></p>
-                    <p style="color:white;"><?php echo get_theme_mod('service_image_2_phone', '+33 1 53 31 25 23'); ?></p>
-                    <p style="color:white;"><?php echo get_theme_mod('service_image_2_email', 'info@esgi.com'); ?></p>
+                    <p ><?php echo get_theme_mod('service_image_2_title', 'Manager'); ?></p>
+                    <p ><?php echo get_theme_mod('service_image_2_phone', '+33 1 53 31 25 23'); ?></p>
+                    <p ><?php echo get_theme_mod('service_image_2_email', 'info@esgi.com'); ?></p>
                 </div>
             </div>
             <div class="col-md-3">
                 <img src="<?php echo get_theme_mod('service_image_3'); ?>" alt="Image 3">
                 <div class="service-info">
-                    <p style="color:white;"><?php echo get_theme_mod('service_image_3_title', 'Manager'); ?></p>
-                    <p style="color:white;"><?php echo get_theme_mod('service_image_3_phone', '+33 1 53 31 25 23'); ?></p>
-                    <p style="color:white;"><?php echo get_theme_mod('service_image_3_email', 'info@esgi.com'); ?></p>
+                    <p ><?php echo get_theme_mod('service_image_3_title', 'Manager'); ?></p>
+                    <p ><?php echo get_theme_mod('service_image_3_phone', '+33 1 53 31 25 23'); ?></p>
+                    <p ><?php echo get_theme_mod('service_image_3_email', 'info@esgi.com'); ?></p>
                 </div>
             </div>
             <div class="col-md-3">
                 <img src="<?php echo get_theme_mod('service_image_4'); ?>" alt="Image 4">
                 <div class="service-info">
-                    <p style="color:white;"><?php echo get_theme_mod('service_image_4_title', 'Manager'); ?></p>
-                    <p style="color:white;"><?php echo get_theme_mod('service_image_4_phone', '+33 1 53 31 25 23'); ?></p>
-                    <p style="color:white;"><?php echo get_theme_mod('service_image_4_email', 'info@esgi.com'); ?></p>
+                    <p ><?php echo get_theme_mod('service_image_4_title', 'Manager'); ?></p>
+                    <p ><?php echo get_theme_mod('service_image_4_phone', '+33 1 53 31 25 23'); ?></p>
+                    <p ><?php echo get_theme_mod('service_image_4_email', 'info@esgi.com'); ?></p>
                 </div>
             </div>
         </div>
     </div>
 </section>
 
-
-<section class="partners-section">
-    <div class="container">
-        <h4><?php echo get_theme_mod('partners_title', 'Nos Partenaires'); ?></h4>
-        <div class="row">
-            <div class="col-md-2">
-                <img src="<?php echo get_theme_mod('partner_logo_1'); ?>" alt="Logo 1">
-            </div>
-            <div class="col-md-2">
-                <img src="<?php echo get_theme_mod('partner_logo_2'); ?>" alt="Logo 2">
-            </div>
-            <div class="col-md-2">
-                <img src="<?php echo get_theme_mod('partner_logo_3'); ?>" alt="Logo 3">
-            </div>
-            <div class="col-md-2">
-                <img src="<?php echo get_theme_mod('partner_logo_4'); ?>" alt="Logo 4">
-            </div>
-            <div class="col-md-2">
-                <img src="<?php echo get_theme_mod('partner_logo_5'); ?>" alt="Logo 5">
-            </div>
-            <div class="col-md-2">
-                <img src="<?php echo get_theme_mod('partner_logo_6'); ?>" alt="Logo 6">
-            </div>
-        </div>
-    </div>
-</section>
 
 <?php get_footer(); ?>
